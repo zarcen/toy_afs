5088f37126c7a71e9608aff3105be484d2c74f11
zarcen <zarcen@gmail.com>
2015-10-31 16:07:16 -0600

    test access

--- tafs/fusexmp.c
+++ tafs/fusexmp.c
@@ -66,6 +66,8 @@ static int xmp_getattr(const char *path, struct stat *stbuf) {
 static int xmp_access(const char *path, int mask)
 {
     printf("## START ## xmp_access\n");
+    return 0;
+    /*
 	int res;
     std::string cpp_path = path;
 	res = greeter->Access(cpp_path, mask);
@@ -73,6 +75,7 @@ static int xmp_access(const char *path, int mask)
 		return -errno;
 
 	return res;
+	*/
 }
 
 static int xmp_readlink(const char *path, char *buf, size_t size)
