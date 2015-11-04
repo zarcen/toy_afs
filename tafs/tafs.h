bcc27df914058f6fe4157d17e06c0a0da9ebe4e2
lcchu <lchu4@wisc.edu>
2015-11-04 14:27:03 -0700

    fixed wrong protoc

--- tafs/tafs.h
+++ tafs/tafs.h
@@ -68,7 +68,7 @@ class GreeterClient {
         }
     }
 
-    // put file in buf
+    /*
     int Read(const std::string& path, std::string& buf, int size, int offset) {
         ReadReq request;
         request.set_path(path);
@@ -86,6 +86,7 @@ class GreeterClient {
             return -1;
         }
     }
+    */
 
     // put file in buf
     int ReadS(const std::string& path, std::string& buf, int size, int offset) {
@@ -180,7 +181,7 @@ class GreeterClient {
         return status.ok() ? reply.err() : -1;
     }
 
-
+    /*
     int Write(const std::string& path, std::string& data, int size, int offset) {
         WriteReq request;
         request.set_path(path);
@@ -194,12 +195,13 @@ class GreeterClient {
 
         return status.ok() ? reply.num_bytes() : -1;
     }
+    */
 
     int WriteS(const std::string& path, std::string& data, int size, int offset) {
-        WriteReq request;
-        WriteReply reply;
+        WriteSReq request;
+        WriteSReply reply;
         ClientContext context;
-        std::unique_ptr<ClientWriter<WriteReq> > writer(
+        std::unique_ptr<ClientWriter<WriteSReq> > writer(
                 stub_->WriteS(&context, &reply));
         int remain = size;
         int stump = 1048576; // 1Mb
